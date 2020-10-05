d633cc194fd3055f104524463110ba72ed9b3b1b
braydentw <reshiramfire130@gmail.com>
2020-10-05 09:54:54 -0500

    Fixed package.json homepage error

---
+++ react-emoji-search/precache-manifest.82151ca0c3601750eb16fd2fb66fbfa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4d197ca3f7af5b12fd721450dbdb18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "924cc0ccffd47e613ef7",
+    "url": "./static/css/main.4edf9be3.chunk.css"
+  },
+  {
+    "revision": "59bb216926123d3a2c54",
+    "url": "./static/js/2.adfbd410.chunk.js"
+  },
+  {
+    "revision": "be795b40fe92a203dce7ffba6d6e7fe7",
+    "url": "./static/js/2.adfbd410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924cc0ccffd47e613ef7",
+    "url": "./static/js/main.4085001c.chunk.js"
+  },
+  {
+    "revision": "25fcdbe3c63aa6142315",
+    "url": "./static/js/runtime-main.ec957825.js"
+  }
+]);
--- react-emoji-search/service-worker.js
+++ react-emoji-search/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/emoji-search/precache-manifest.158f4205a90adb77da5541f8d34ce1fd.js"
+  "./precache-manifest.82151ca0c3601750eb16fd2fb66fbfa6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/emoji-search/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

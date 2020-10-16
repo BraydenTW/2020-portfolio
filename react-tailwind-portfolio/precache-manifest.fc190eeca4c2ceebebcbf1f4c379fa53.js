2317f4ac5687b8d8ffc7ff08e65d5ef74904be24
braydentw <reshiramfire130@gmail.com>
2020-10-16 12:42:31 -0500

    Added react-tailwind-portfolio

---
+++ react-tailwind-portfolio/precache-manifest.fc190eeca4c2ceebebcbf1f4c379fa53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae21a4e6d13a30482574bc2141bb319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e3841e2c911ceae4a8c",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "31c18c10ab93183f3c94",
+    "url": "/static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "6e3841e2c911ceae4a8c",
+    "url": "/static/js/2.5f9a82f7.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "/static/js/2.5f9a82f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c18c10ab93183f3c94",
+    "url": "/static/js/main.4a86c38e.chunk.js"
+  },
+  {
+    "revision": "c044c296fef9e582399f",
+    "url": "/static/js/runtime-main.4070baf2.js"
+  },
+  {
+    "revision": "7c4aed56245ae966a682cb5ab46dee43",
+    "url": "/static/media/profile.7c4aed56.png"
+  }
+]);

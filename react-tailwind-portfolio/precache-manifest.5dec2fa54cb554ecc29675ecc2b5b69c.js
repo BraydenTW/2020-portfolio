3b687934b724259db49f3f17dc1256b6a9f71a10
braydentw <reshiramfire130@gmail.com>
2020-10-16 12:48:08 -0500

    Fixed package.json homepage bug

---
+++ react-tailwind-portfolio/precache-manifest.5dec2fa54cb554ecc29675ecc2b5b69c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9f11c57d7970d251aa268b2353e487",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e3841e2c911ceae4a8c",
+    "url": "./static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "31c18c10ab93183f3c94",
+    "url": "./static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "6e3841e2c911ceae4a8c",
+    "url": "./static/js/2.5f9a82f7.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "./static/js/2.5f9a82f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c18c10ab93183f3c94",
+    "url": "./static/js/main.4a86c38e.chunk.js"
+  },
+  {
+    "revision": "d25113d24f590e04e053",
+    "url": "./static/js/runtime-main.80763978.js"
+  },
+  {
+    "revision": "7c4aed56245ae966a682cb5ab46dee43",
+    "url": "./static/media/profile.7c4aed56.png"
+  }
+]);
